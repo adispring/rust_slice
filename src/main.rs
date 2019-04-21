cb5ac16b2962c8e5e2f2d7368a7f131f166053e8
wangzengdi <sunnyadi@163.com>
2019-04-21 19:13:34 +0800

    use slice to find first word

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 fn main() {
     let s = String::from("hello world");
 
-    let hello = &s[0..5];
-    let world = &s[6..11];
+    let word = first_word(&s);
+    println!("word: {}", word);
+}
 
-    println!("{}, {}", hello, world);
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
